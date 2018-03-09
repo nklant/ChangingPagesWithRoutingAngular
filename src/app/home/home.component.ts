c67b50fe7f1ceeeb60fa73c39b483e6fe6496411
Nikolay Antonov <nikolay.antonov@globalcons.com>
2018-03-09 15:40:25 +0200

    137. Using Fake Auth Service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
   }
@@ -17,4 +18,13 @@ export class HomeComponent implements OnInit {
     this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
   }
 
+  onLogin() {
+    this.authService.login();
+    console.log('You logged in!');
+  }
+
+  onLogout() {
+    this.authService.logout();
+    console.log('You logged out!');
+  }
 }
