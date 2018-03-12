5bf0148322411c1b36435b088aa30dbaad51e413
Nikolay Antonov <nikolay.antonov@globalcons.com>
2018-03-12 10:40:20 +0200

    141. Understanding Location Strategies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,8 @@ import { UsersComponent } from './users/users.component';
 import { HomeComponent } from './home/home.component';
 import { AuthGuard } from './auth-guard.service';
 import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { ServerResolver } from './servers/server/server-resolver.service';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
@@ -22,16 +24,18 @@ const appRoutes: Routes = [
       canActivateChild: [AuthGuard],
       component: ServersComponent, 
       children: [
-        { path: ':id', component: ServerComponent },
+        { path: ':id', component: ServerComponent, resolve: {server: ServerResolver} },
         { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }
       ]
     },
-    { path: 'not-found', component: PageNotFoundComponent},
+    // { path: 'not-found', component: PageNotFoundComponent},
+    { path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not found!'}},
     { path: '**', redirectTo: '/not-found'}
   ];
 
 @NgModule({
     imports: [
+        // RouterModule.forRoot(appRoutes, {useHash: true}) // For older server/browser support
         RouterModule.forRoot(appRoutes)
     ],
     exports: [RouterModule]
